0d4a030259e9d216bb0c4e6fdb9e53779c3e677a
kee236 <31869356+kee236@users.noreply.github.com>
2024-09-10 22:24:09 +0700

    Update webhook.sql

--- Db/webhook.sql
+++ Db/webhook.sql
@@ -26,3 +26,17 @@ CREATE TABLE IF NOT EXISTS `messenger_bot_thirdparty_webhook_activity` (
   PRIMARY KEY (`id`),
   FOREIGN KEY (`webhook_id`) REFERENCES `messenger_bot_thirdparty_webhook` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS `messenger_bot_thirdparty_webhook_trigger` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `webhook_id` int(11) NOT NULL, 
+  `trigger_type` varchar(50) NOT NULL,
+  `trigger_value` varchar(255) NOT NULL,
+  `comparison_operator` varchar(10) NOT NULL,
+  `trigger_data` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`webhook_id`) REFERENCES `messenger_bot_thirdparty_webhook` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
