19349bd455a32dd381cd6d83c48740d753ab28a4
kee236 <31869356+kee236@users.noreply.github.com>
2025-04-22 18:38:41 +0700

    Create partial/footer.php

---
+++ public_html/application/views/site/template1/partial/footer.php
@@ -0,0 +1,70 @@
+<footer class="footer section pt-6 pt-md-8 pt-lg-10 pb-3 bg-primary text-white overflow-hidden">
+    <div class="pattern pattern-soft top"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <a class="footer-brand mr-lg-5 d-flex" href="<?php echo base_url(); ?>">
+                    <img src="<?php echo base_url('front/assets/img/brand/light.svg'); ?>" height="35" class="mr-3" alt="Footer logo">
+                </a>
+                <p class="my-4">Create, prototype, collaborate and turn your ideas into incredible products with the definitive platform for digital design.</p>
+                <div class="dropdown mb-4 mb-lg-0">
+                    <a id="langsDropdown" href="                                                                                                                  
+                        <img src="<?php echo base_url('front/assets/img/flags/united-states-of-america.svg'); ?>" alt="USA Flag" class="language-flag"> English <i class="fas fa-chevron-down ml-1"></i>
+                    </a>
+                    <div aria-labelledby="langsDropdown" class="dropdown-menu dropdown-menu-center">
+                        <a href="#" class="dropdown-item text-gray text-sm"><img src="<?php echo base_url('front/assets/img/flags/thailand.svg'); ?>" alt="Thailand Flag" class="language-flag">ภาษาไทย </a>
+                        <a href="                                                                                                                                                                            
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 col-sm-3 col-lg-2 mb-4 mb-lg-0">
+                <h6>Ismart Ai</h6>
+                <ul class="links-vertical">
+                    <li><a target="_blank" href="<?php echo base_url('blog'); ?>">Blog</a></li>
+                    <li><a target="_blank" href="<?php echo base_url('futures'); ?>">Products</a></li>
+                    <li><a target="_blank" href="<?php echo base_url('contact'); ?>">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-sm-3 col-lg-2 mb-4 mb-lg-0">
+                <h6>Learn More</h6>
+                <ul class="links-vertical">
+                    <li><a target="_blank" href="<?php echo base_url('getting-started'); ?>">Getting Started</a></li>
+                    <li><a target="_blank" href="<?php echo base_url('api-documentation'); ?>">API Documentation</a></li>
+                    <li><a target="_blank" href="<?php echo base_url('pricing'); ?>">Pricing</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-4">
+                <h6>Subscribe</h6>
+                <p class="font-small">The latest Impact news, articles, and resources, sent straight to your inbox every month.</p>
+                <form action="<?php echo base_url('subscribe'); ?>" method="post">
+                    <div class="form-row">
+                        <div class="col-8">
+                            <input type="email" class="form-control mb-2" placeholder="Email Address" name="email" required>
+                        </div>
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-secondary btn-block"><span>ติดตาม</span></button>
+                        </div>
+                    </div>
+                </form>
+                <small class="mt-2 form-text">We’ll never share your details. See our <a href="<?php echo base_url('privacy-policy'); ?>" class="font-weight-bold text-underline">Privacy Policy</a></small>
+            </div>
+        </div>
+        <hr class="my-4 my-lg-5">
+        <div class="row">
+            <div class="col pb-4 mb-md-0">
+                <div class="d-flex text-center justify-content-center align-items-center">
+                    <p class="font-weight-normal mb-0">ติดตามเรา</p>
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item"><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                        <li class="list-inline-item"><a href="                                                            
+                        <li class="list-inline-item"><a href="                                                         
+<li class="list-inline-item"><a href="#" target="_blank"><i class="fab fa-youtube"></i></a></li>
+<li class="list-inline-item"><a href="                                                          
+<li class="list-inline-item"><a href="#" target="_blank"><i class="fab fa-line"></i></a></li>
+</ul>
+</div>
+<p class="font-weight-normal mb-0 text-center">&copy; <?php echo date('Y'); ?> Ismart Ai. All Rights Reserved.</p>
+</div>
+</div>
+</div>
+</footer>
